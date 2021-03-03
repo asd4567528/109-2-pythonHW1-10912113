1ec46f4c449d4b627ffecee1944359a3796a6d75
asd4567528 <79976688+asd4567528@users.noreply.github.com>
2021-03-03 22:01:23 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myArithmetic,myCalcArea,myStatistics
+N1 = float(input("the first number:"))
+N2 = float(input("the second number:"))
+N3 = float(input("the third number:"))
+N4 = float(input("the fourth number:"))
+N5 = float(input("the fifth number:"))
+List = [N1,N2,N3,N4,N5]
+print(myStatistics.myMean(List))
